8fa481b9e36600ebf1e6d0fb268c4b1cd81edb31
Sho Takahashi <sho.takahashi@gmail.com>
2013-03-21 09:19:46 -0700

    removed comma from recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         #recoverable--this is for recoverying password
-        , :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable #recoverable--this is for recoverying password
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
